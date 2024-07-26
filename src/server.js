b5a90958b2de4f2f09aed642c463afc6787e6f7a
pqv1991 <pqv1991@gmail.com>
2024-07-26 18:43:36 +0700

    create models seeds

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from 'body-parser';
+//import connection from "./config/connectDB";
 
 
 const PORT = process.env.PORT || 8080;
@@ -12,6 +13,9 @@ configViewEngine(app);
 //body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connecttion
+//connection();
 // // init web routes
 initWebRoutes(app);
 
